fa794aac4355fd062f91c171f53b597f94f0309a
Mohamad Almoussa <mohamad_almoussa@hotmail.com>
2020-11-14 04:01:59 +0100

    form builder for registration start

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { user } from '../models/user';
 import { RegisterService } from '../services/register/register.service';
 
 @Component({
@@ -8,18 +10,35 @@ import { RegisterService } from '../services/register/register.service';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private registerService: RegisterService) { }
-
+  constructor(private fb: FormBuilder,private registerService: RegisterService) { }
+  registrationForm: FormGroup;
+  regUser: user;
   ngOnInit(): void {
+
+    this.registrationForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confPassword: ['', [Validators.required]],
+      telNumber: ['', [Validators.required]],
+      age: ['', [Validators.required]],
+      sex: ['', [Validators.required]],
+    }
+    );
   }
 
 
  public  register():any{
     console.log("register");
-    this.registerService.register().then(response=>{
+//TODO: fix the value issue by adding labels
+//TODO: add conditions for the input values
+  this.regUser=  this.registrationForm.getRawValue();
+      console.log(this.regUser.firstName)
+    // this.registerService.register().then(response=>{
 
-      console.log(response.body)
-    })
+    //   console.log(response.body)
+    // })
 
   }
 }
